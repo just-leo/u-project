ce12f785574b0e36f8238b405f8d7a7f04de075e
just-leo <leonid3g@gmail.com>
2016-12-01 16:27:03 +0200

    Persist selected language in localstorage

--- client/config/app.config.js
+++ client/config/app.config.js
@@ -4,7 +4,8 @@ angular
   .module('app.config', [
     'app.config.autoload',
     'app.config.api',
-    'app.config.routes'
+    'app.config.routes',
+    'LocalStorageModule'
   ])
   .config(['$locationProvider', function($locationProvider){
     $locationProvider.html5Mode({
@@ -18,18 +19,24 @@ angular
   .config(['$compileProvider', function ($compileProvider) {
     $compileProvider.debugInfoEnabled(app.DEBUG);
   }])
+  .constant('I18N_KEY', 'i18n')
   .constant('availableLanguages', {
     en: 'en-US,en;q=0.8',
     ru: 'ru-RU,ru'
   })
   .config(function($httpProvider, availableLanguages) {
-      $httpProvider.defaults.headers.common['Accept-Language'] = availableLanguages['en'];
+    $httpProvider.defaults.headers.common['Accept-Language'] = availableLanguages['en'];
   })
-  .run(function($rootScope, availableLanguages, $http){
-      $rootScope.$on('languageChanged', function(event, lang){
-        if(availableLanguages[lang]) {
-          $http.defaults.headers.common['Accept-Language'] = availableLanguages[lang]
-        }
-        console.log('Switched to', availableLanguages[lang]);
-      })
-    });
+  .run(function($rootScope, availableLanguages, $http, localStorageService, I18N_KEY){
+    //Resolves language and setup header
+    var selectedLanguage = localStorageService.get(I18N_KEY) || availableLanguages['en'];
+    $http.defaults.headers.common['Accept-Language'] = selectedLanguage;
+
+    $rootScope.$on('languageChanged', function(event, lang){
+      if(availableLanguages[lang]) {
+        $http.defaults.headers.common['Accept-Language'] = availableLanguages[lang];
+        localStorageService.set(I18N_KEY, availableLanguages[lang]);
+      }
+      console.log('Switched to', availableLanguages[lang]);
+    })
+  });
