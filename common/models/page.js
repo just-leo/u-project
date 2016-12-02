5138b8661ff467637175e798192550525c20d529
just-leo <leonid3g@gmail.com>
2016-12-02 03:20:01 +0200

    Hidden unused remote methods for Page model

--- common/models/page.js
+++ common/models/page.js
@@ -6,12 +6,6 @@ module.exports = function(Page) {
 
   Page.validatesUniquenessOf('name', {message: 'Page name is not unique'});
 
-  //deny access to some public apis
-  Page.disableRemoteMethod('deleteById', true); // DELETE
-  Page.disableRemoteMethod('create', true); // POST
-  Page.disableRemoteMethod('upsert', true); // PUT
-  Page.disableRemoteMethod('updateAll', true); // POST
-
   Page.afterRemote('findOne', function(context, remoteMethodOutput, next) {
     var language = context.req.language;//contains user language
 
